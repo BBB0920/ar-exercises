662c230194bf33473734dac27a1d19212b3732ea
Lyon <lyon.b.lee@gmail.com>
2023-03-09 19:47:42 -0500

    W09D01 - More AR - Stores and Employees, 4th Exercise complete, Create another three stores in the same way as 1st Exercise, then filter and puts men only store with annual revenue, and separately, filter and puts women only store that makes less than 1,000,000 annually

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# users = User.where(name: 'David', occupation: 'Code Artist').order(created_at: :desc)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+for men_store in @mens_stores 
+  puts "#{men_store.name} generates $#{men_store.annual_revenue} per year."
+end
+
+@women_stores = Store.where(mens_apparel: false, womens_apparel: true, annual_revenue: 0..1000000)
+for women_store in @women_stores
+  puts "#{women_store.name} generates $#{women_store.annual_revenue} per year."
+end
