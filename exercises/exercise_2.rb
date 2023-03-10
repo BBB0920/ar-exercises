dfdf19c282e134ac76f751d79951996a108ff2fc
Lyon <lyon.b.lee@gmail.com>
2023-03-09 21:26:03 -0500

    W09D01 - More AR - Stores and Employees, 7th Exercise completed, Create validation for Stores and Employees, and take user input (intentionally creating a validation error) and display errors from ActiveRecord

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,7 +4,7 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here ...$t
 
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
